3d3f04f06d5e093886ae69b26f945c103148e1e5
Christopher Lee <chris@codestorming.local>
2010-10-08 13:56:47 -0500

    show all buckets

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
   before_filter :login_required
   
   def index
-    @buckets = current_user.buckets
+    @buckets = Bucket.all(:order => "name ASC")
     @posts = Post.find(:all, :order => "created_at DESC")
   end
 end
