a9ad612d004b4783b137ad257137471553643409
Christopher Lee <chris@codestorming.local>
2010-10-09 00:56:52 -0500

    try to handle nil scores

--- app/models/vote_counter_observer.rb
+++ app/models/vote_counter_observer.rb
@@ -4,10 +4,12 @@ class VoteCounterObserver < ActiveRecord::Observer
   def after_create(vote)
     if vote.post
       post = vote.post
+      post.score_counter = 0 if post.score_counter.nil?
       post.score_counter += vote.score
       post.save
     elsif vote.comment
       comment = vote.comment
+      comment.score_counter = 0 if comment.score_counter.nil?
       comment.score_counter += vote.score
       comment.save
     end
