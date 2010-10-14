d237885b1914907395609940760a7fb37ca95346
Christopher Lee <chris@codestorming.local>
2010-10-14 11:10:07 -0500

    scope to account

--- app/controllers/admin/site_configurations_controller.rb
+++ app/controllers/admin/site_configurations_controller.rb
@@ -1,10 +1,10 @@
 class Admin::SiteConfigurationsController < Admin::ApplicationController
   def show
-    @buckets = Bucket.find(:all, :order => "name ASC")
+    @buckets = current_account.buckets.find(:all, :order => "name ASC")
   end
   
   def update
-    @buckets = Bucket.find(:all, :order => "name ASC")
+    @buckets = current_account.buckets.find(:all, :order => "name ASC")
     if @site_configuration.update_attributes(params[:site_configuration])
       flash[:notice] = "Your settings have been saved."
       redirect_to admin_site_configuration_url
