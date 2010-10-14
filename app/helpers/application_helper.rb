3471d2dbf5246beb6c1300b02a9bedc864cd0188
Christopher Lee <chris@codestorming.local>
2010-10-14 18:43:43 -0500

    autolink text

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def options_for_buckets(buckets = [], selected = nil)
     options_for_select(buckets.collect{ |bucket| [ bucket.name, bucket.id ]}, selected)
   end
+  
+  def safely_formatted_text(string = "", css_class = "")
+    auto_link(simple_format(strip_tags(string), :class => css_class), :html => { :rel => "nofollow", :target => "_blank"})
+  end
 end
